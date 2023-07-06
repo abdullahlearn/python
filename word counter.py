64d575f79008b94adb49547511fc14d4d8892b7a
abdullahlearn <137713507+abdullahlearn@users.noreply.github.com>
2023-07-06 23:09:25 +0500

    added some tests

--- word counter.py
+++ word counter.py
@@ -1,16 +1,16 @@
 introduction='''Welcome to the "Word Counter" app.
              In this app, you'll enter a sentence, 
              and we'll respond with the number of words
-               you've typed in your sentence.
+               you've typed in your conviction.
                  Would you like to play this game?
                    (y/n):'''
 print(introduction)
 answer=input()
 if answer =="y" or answer =="yes":
     print("ok , let's do this.")
-    sentence=input("")
-    countOfWords = len(sentence.split())
-    print("Count of Words in the given Sentence:", countOfWords)
+    sentence=input("write your sentence here:")
+    count words = len(sentence.split())
+    print("Count of Words in the given Sentence:", Count words)
     print("see you next time.......")
 else:
     print("ok  , no problem")
