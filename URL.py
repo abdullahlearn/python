f8e2998baa9e00910b0112a7517c2a3825e05389
Abullah <abdullahmuhammad.one@gmail.com>
2023-10-28 23:02:08 +0500

    used random commands

---
+++ URL.py
@@ -0,0 +1,9 @@
+introduction='''Welcome to the 'picture opening' app
+              In this app you'll enter a URL of a 
+              picture or anything else and then we'll
+              open it.'''
+print(introduction)
+import webbrowser
+Url=input()
+webbrowser.open(Url)
+print("see you next time.............")
